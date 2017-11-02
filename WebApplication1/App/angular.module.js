38c16d83d2567a588380700b5b560cc722324edf
Ryan Grant WLU <grantr16@mail.wlu.edu>
2017-11-01 20:45:46 -0400

    initial commit t quit
    asidaolk
    X
    
    commit pls
    git commit
    git push
    
     Please enter the commit message for your changes. Lines starting
     with '#' will be ignored, and an empty message aborts the commit.
    n branch master

---
+++ WebApplication1/App/angular.module.js
@@ -0,0 +1,46 @@
+﻿var app = angular.module("App", []);
+
+app.controller("mainController",
+    function ($scope) {
+        $scope.toTitanArmor = function () {
+            //this needs to be a component thing
+        };
+        $scope.weaponList = [
+            {
+                name: "Rat King",
+                inactiveImage: "nyi",
+                activeImage: "nyi",
+                displayImage: inactiveImage,
+                active: false,
+                location: "Some questline"
+            },
+            {
+                name: "MIDA Multi Tool",
+                inactiveImage: "nyi",
+                activeImage: "nyi",
+                displayImage: inactiveImage,
+                active: false,
+                location: "Some other questline"
+            }
+        ];
+        $scope.titanArmorList = [
+            {
+                name: "ACDO Feedback",
+                inactiveImage: "nyi",
+                activeImage: "nyi",
+                displayImage: inactiveImage,
+                active: false,
+                location: "Random"
+            }
+        ];
+    }
+);
+
+app.controller("yourController",
+    function ($scope) {
+        $scope.yourWeaponList = $scope.weaponList;
+        $scope.activateButton = function (index) {
+            yourWeaponList[index].active = !(yourWeaponList[index].active);
+            yourWeaponList[index].displayImage = 
+        };
+    })
