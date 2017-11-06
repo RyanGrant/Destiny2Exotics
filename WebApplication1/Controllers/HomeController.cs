f6464c3e826a99f67c00f473826fb156d6da4fe6
Ryan Grant WLU <grantr16@mail.wlu.edu>
2017-11-06 09:08:32 -0500

    Attempting to call API from C#

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,6 +12,7 @@ namespace WebApplication1.Controllers
     {
         public ActionResult Index()
         {
+            api();
             return View();
         }
 
@@ -20,11 +23,36 @@ namespace WebApplication1.Controllers
             return View();
         }
 
-        public ActionResult Contact()
+        public void api()
         {
-            ViewBag.Message = "Your contact page.";
 
-            return View();
+            string html = string.Empty;
+
+            //WebClient client = new WebClient();
+            String apiKey = "1b3f55b792004e41b894bd035ab93b3d";
+            //client.Headers.Add("X-API-Key", apiKey);
+            //request.Headers["X-My-Custom-Header"] = "the-value";
+            WebRequest webRequest = WebRequest.Create("http://www.bungie.net/Platform/Destiny2/Stats/Definition");
+            webRequest.Method = "GET";
+            webRequest.Timeout = 12000;
+            webRequest.ContentType = "application/json";
+            webRequest.Headers.Add("X-API-Key", apiKey);
+            String result = "";
+            using (System.IO.Stream s = webRequest.GetResponse().GetResponseStream())
+            {
+                using (System.IO.StreamReader sr = new System.IO.StreamReader(s))
+                {
+                    var jsonResponse = sr.ReadToEnd();
+                    result += jsonResponse;
+                    Console.WriteLine(String.Format("Response: {0}", jsonResponse));
+                }
+            }
+           
+
+            //var fantasyAPI = $http.get("http://www.bungie.net/Platform/Destiny2/Stats/Definition",
+              ///      { headers: { 'X-API-Key': apiKey } });
+
         }
+
     }
 }
